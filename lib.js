0b5819cdb32bc5ec2226c1487a1d65778904f1a5
Luke Salisbury <dev@lukesalisbury.name>
2021-01-03 17:48:32 +1100

    Update lib.js

--- lib.js
+++ lib.js
@@ -6,19 +6,28 @@ function blockMedia(mediaElement, dynamic = false) {
 		mediaElement.addEventListener('play', watchForPlayEvent, true );
 		mediaElement.addEventListener('playing', watchForPlayEvent, true );
 		mediaElement.addEventListener('mouseover', unblockElement );
+		mediaElement.addEventListener('mouseenter', unblockElement );
+		mediaElement.addEventListener('contextmenu', unblockElement );
 		if ( !mediaElement.hasAttribute('playsinline') ) {
 			mediaElement.setAttribute('preload', 'metadata');
+			mediaElement.dataset.blockElementId = 1;
+
 		} else {
 			// playsinline - Likely used as background, so don't do a click to play 
-			console.log('Disabled a media background', mediaElement, dynamic);
+			console.log('Disabled a media background', mediaElement, 'dynamic', dynamic, mediaElement.parentElement);
 			mediaElement.setAttribute('preload', 'none');
+			mediaElement.dataset.blockElementId = 2;
+			if( dynamic ) {
+				mediaElement.parentElement.addEventListener('mouseenter', e => {
+					mediaElement.dataset.unblockElementId = mediaElement.currentSrc;
+				} );
+			}
 		}
-		mediaElement.dataset.blockElementId = 1;
 	} 
 	
 };
 function resetUnblockElement(mediaElement) {
-	delete  mediaElement.dataset.unblockElementId;
+	delete mediaElement.dataset.unblockElementId;
 }
 function unblockElement(event) {
 	event.target.dataset.unblockElementId = event.target.currentSrc;
@@ -27,6 +36,7 @@ function unblockElement(event) {
 function watchForPlayEvent(event) {
 	event.preventDefault();
 	if (!this.dataset.unblockElementId || this.dataset.unblockElementId != event.target.currentSrc) {
+		console.log('Unblock id not set', event.target);
 		this.pause()
 	}
 	return false;
